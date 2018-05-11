f32c42b58d0eacf88ba1b0448195b57bd21b37d5
ermondel <ermondel@gmail.com>
2018-05-11 18:41:51 +0300

    add reactions to changes in form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react'
 import Header from './components/header/'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            city: '',
+            period: 3,
+            isCelsius: true,
+        }
+
+        this.onSubmitSearch = this.onSubmitSearch.bind(this)
+        this.onChangePeriod = this.onChangePeriod.bind(this)
+        this.onChangeUnit   = this.onChangeUnit.bind(this)
+    }
+
+    onSubmitSearch(event) {
+        event.preventDefault()
+        this.setState({ city: event.target.elements.city.value })
+    }
+
+    onChangePeriod(event) {
+        this.setState({ period: Number(event.target.value) })
+    }
+
+    onChangeUnit(event) {
+        this.setState({ isCelsius: event.target.value === 'celsius' ? true : false })
+    }
+
     render() {
         return (
             <div>
-                <Header />
+                <Header
+                    onSubmitSearch={ this.onSubmitSearch }
+                    onChangePeriod={ this.onChangePeriod }
+                    onChangeUnit={ this.onChangeUnit }
+                    city={ this.state.city }
+                    period={ this.state.period }
+                    isCelsius={ this.state.isCelsius }
+                />
             </div>
         )
     }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,42 +2,61 @@ import React, { Component } from 'react'
 
 class Header extends Component {
     render() {
-        const city = ''
+		const { onSubmitSearch, onChangePeriod, onChangeUnit } = this.props
+		const { city, period, isCelsius } = this.props
 
         return (
 		<header>
 			<div id="header-inner">
 				<h1>Weather forecast for the city up to 2 weeks</h1>
-				<form id="form">
+				<form id="form" onSubmit={onSubmitSearch}>
 		    		<div className="left">
 			    		<div id="search-box">
 				    		<label htmlFor="city" className="visuallyhidden" tabIndex="0">Choose the city of your interest</label>
-				    		<input type="text" name="city" id="city" value={city} placeholder="e.g. New York" required />
+				    		<input type="text" name="city" id="city" defaultValue={city} placeholder="e.g. New York" required />
 			    		</div>
 						<div id="options">
-						<div id="options-period">
-							<label className="common-radio" tabIndex="0">
-								<input type="radio" name="period" value="1" required /><span>day</span>
-        					</label>
-        					<label className="common-radio" tabIndex="0">
-            					<input type="radio" name="period" value="3" required /><span>3 days</span>
-        					</label>
-        					<label className="common-radio" tabIndex="0">
-            					<input type="radio" name="period" value="7" required /><span>week</span>
-        					</label>
-        					<label className="common-radio" tabIndex="0">
-            					<input type="radio" name="period" value="14" required /><span>2 weeks</span>
-        					</label>
+							<div id="options-period">
+								<label className="common-radio" tabIndex="0">
+									<input type="radio" name="period" value="1" required 
+									checked={period === 1} 
+									onChange={onChangePeriod} />
+									<span>day</span>
+        						</label>
+        						<label className="common-radio" tabIndex="0">
+									<input type="radio" name="period" value="3" required 
+									checked={period === 3} 
+									onChange={onChangePeriod} />
+									<span>3 days</span>
+        						</label>
+        						<label className="common-radio" tabIndex="0">
+									<input type="radio" name="period" value="7" required 
+									checked={period === 7} 
+									onChange={onChangePeriod} />
+									<span>week</span>
+        						</label>
+        						<label className="common-radio" tabIndex="0">
+									<input type="radio" name="period" value="14" required 
+									checked={period === 14} 
+									onChange={onChangePeriod} />
+									<span>2 weeks</span>
+        						</label>
+        					</div>
+        					<div id="options-unit">
+        						<label className="common-radio" tabIndex="0">
+									<input type="radio" name="unit" value="celsius" required
+									checked={isCelsius}
+									onChange={onChangeUnit} />
+									<span>Celsius</span>
+        						</label>
+        						<label className="common-radio" tabIndex="0">
+									<input type="radio" name="unit" value="fahrenheit" required
+									checked={!isCelsius}
+									onChange={onChangeUnit} />
+									<span>Fahrenheit</span>
+        						</label>
+        					</div>
         				</div>
-        				<div id="options-unit">
-        					<label className="common-radio" tabIndex="0">
-            					<input type="radio" name="unit" value="celsius" required /><span>Celsius</span>
-        					</label>
-        					<label className="common-radio" tabIndex="0">
-            					<input type="radio" name="unit" value="fahrenheit" required /><span>Fahrenheit</span>
-        					</label>
-        				</div>
-        			</div>
 					</div>
 					<div className="right">
 						<input type="submit" value="Submit" id="submit" tabIndex="0" />
